628cdd3588c444f4ec1028af7aa3a3e37e46262e
David Revelo <drevelo@softconsulting.com.ec>
2019-10-30 15:13:16 -0500

    chat carga
    Carga
    chat V1

---
+++ Chat Vehiculos/public/js/Conexion/credencialesWex.js
@@ -0,0 +1,30 @@
+//a316cd03-f42e-4f6b-9cac-cdc5fa1deb22 
+//fd511920-1e9c-45d2-9fe8-d8824ca56ec6
+
+var tramos={
+    principal:{
+        wconv_version_date : '2018-09-20',
+        wconv_workspaceId : 'd7ff2b06-b0dd-429e-94e1-87e41812c99f',
+        wconv_apikey : 'dPVHWNmEXdRKlP2Q0-0MTXdboSeWfJ0yP5x4HehNUZYq',
+        wconv_url : 'https://gateway.watsonplatform.net/assistant/api',
+    },secundario:{
+    },bd:{
+        config : {
+            "host": "192.168.10.221",
+            "user": "sa",
+            "password": "Soft2019.",
+            "database": "MDCliente"
+            } 
+    },mongo:{
+        url:'mongodb://40.117.183.46:27017/WatsonVehiculos',
+        bd:"ChatTecniseguros",
+        user:"" ,//"amin",
+        pwd:"" //"123"
+    },telegram:{
+        key:"1019697440:AAHHcJ2t_wEbRgWdxEbq4d4ExQ3d1_4oImA"
+    },facebook:{
+        token:"EAAFTZBDzFoS8BAFe9YGIvO1HzX1Fpxuz78jz3BbMZBP8ADnAmftk1XFIiBuZAnNpYSNyI1JzJ1oInI98RBCrArlBFYUktFBtoPcm6mrYWhUyNS6bndnoJ5tZAMevUDmtI1Q3uCQhgiiPeSZBZC2KRM7uON5KvYXDFqjGD8rT7ZCXAZDZD"
+    }
+}
+
+module.exports=tramos;  
