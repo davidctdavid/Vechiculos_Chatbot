51deb40d66ae4832bb2e2ffca8ebcac9792538dd
David Revelo <drevelo@softconsulting.com.ec>
2019-10-31 11:21:50 -0500

    update

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ function buscarUsuario(id){
   console.log("server starting on " + appEnv.url);
 });*/
 
-app.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
